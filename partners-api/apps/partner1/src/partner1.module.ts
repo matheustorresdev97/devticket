d1b5ac0da7e6fd4fe9819974dbe73b622e3b7071
matheustorresdev97 <matheustorresdev@gmail.com>
2024-10-01 18:56:50 -0300

    create different partner 1 and partner2, add api key authentication in reserve spot

---
+++ partners-api/apps/partner1/src/partner1.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from '@app/core/prisma/prisma.module';
+import { AuthModule } from '@app/core/auth/auth.module';
+import { EventsModule } from './events/events.module';
+import { SpotsModule } from './spots/spots.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.partner1',
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+    EventsModule,
+    SpotsModule,
+  ],
+})
+export class Partner1Module {}
