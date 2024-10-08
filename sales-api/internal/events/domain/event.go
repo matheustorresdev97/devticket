711798f069ca9d88873f4e9b1b266f6cbfc3f077
matheustorresdev97 <matheustorresdev@gmail.com>
2024-10-08 08:12:31 -0300

    feat/ implements sales-api go

---
+++ sales-api/internal/events/domain/event.go
@@ -0,0 +1,86 @@
+package domain
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrEventNameRequired     = errors.New("event name is required")
+	ErrEventDateFuture       = errors.New("event date must be in the future")
+	ErrEventCapacityPositive = errors.New("event capacity must be greater than 0")
+	ErrEventPricePositive    = errors.New("event price must be greater than 0")
+	ErrEventNotFound         = errors.New("event not found")
+)
+
+type Rating string
+
+const (
+	RatingLivre Rating = "L"
+	Rating10    Rating = "L10"
+	Rating12    Rating = "L12"
+	Rating14    Rating = "L14"
+	Rating16    Rating = "L16"
+	Rating18    Rating = "L18"
+)
+
+type Event struct {
+	ID           string
+	Name         string
+	Location     string
+	Organization string
+	Rating       Rating
+	Date         time.Time
+	ImageURL     string
+	Capacity     int
+	Price        float64
+	PartnerID    int
+	Spots        []Spot
+	Tickets      []Ticket
+}
+
+func NewEvent(name, location, organization string, rating Rating, date time.Time, capacity int, price float64, imageUrl string, partnerID int) (*Event, error) {
+	event := &Event{
+		ID:           uuid.New().String(),
+		Name:         name,
+		Location:     location,
+		Organization: organization,
+		Rating:       rating,
+		Date:         date,
+		Capacity:     capacity,
+		Price:        price,
+		ImageURL:     imageUrl,
+		PartnerID:    partnerID,
+		Spots:        make([]Spot, 0),
+	}
+	if err := event.Validate(); err != nil {
+		return nil, err
+	}
+	return event, nil
+}
+
+func (e *Event) Validate() error {
+	if e.Name == "" {
+		return ErrEventNameRequired
+	}
+	if e.Date.Before(time.Now()) {
+		return ErrEventDateFuture
+	}
+	if e.Capacity <= 0 {
+		return ErrEventCapacityPositive
+	}
+	if e.Price <= 0 {
+		return ErrEventPricePositive
+	}
+	return nil
+}
+func (e *Event) AddSpot(name string) (*Spot, error) {
+	spot, err := NewSpot(e, name)
+	if err != nil {
+		return nil, err
+	}
+	e.Spots = append(e.Spots, *spot)
+	return spot, nil
+}
---
+++ sales-api/internal/events/domain/ticket.go
@@ -0,0 +1,61 @@
+package domain
+
+import (
+	"errors"
+	"github.com/google/uuid"
+)
+
+type TicketType string
+
+const (
+	TicketTypeFull TicketType = "full"
+	TicketTypeHalf TicketType = "half"
+)
+
+var (
+	ErrTicketPricePositive = errors.New("ticket price must be greater than zero")
+	ErrTicketTypeInvalid   = errors.New("invalid ticket type")
+)
+
+type Ticket struct {
+	ID         string
+	EventID    string
+	Spot       *Spot
+	TicketType TicketType
+	Price      float64
+}
+
+func NewTicket(event *Event, spot *Spot, ticketType TicketType) (*Ticket, error) {
+	if IsValidTicketType(ticketType) {
+		return nil, ErrTicketTypeInvalid
+	}
+	ticket := &Ticket{
+		ID:         uuid.New().String(),
+		EventID:    event.ID,
+		Spot:       spot,
+		TicketType: ticketType,
+		Price:      event.Price,
+	}
+	ticket.CalculatePrice()
+	if err := ticket.Validate(); err != nil {
+		return nil, err
+	}
+	return ticket, nil
+}
+
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeFull || ticketType == TicketTypeHalf
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrTicketPricePositive
+	}
+	return nil
+}
